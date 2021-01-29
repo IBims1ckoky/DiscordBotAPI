ecf5da57060aeb162a18e6f5df447be00284c579
IBims1ckoky <68447458+IBims1ckoky@users.noreply.github.com>
2021-01-29 01:00:51 +0100

    removed token

--- src/main/java/de/maxizink/discordbotapi/Example.java
+++ src/main/java/de/maxizink/discordbotapi/Example.java
@@ -5,15 +5,13 @@ import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 
-import java.util.ArrayList;
-
 @RequiredArgsConstructor
 public class Example {
 
   public static void main(final String[] args) {
     System.out.println("DiscordBotAPI - by @IBims1ckoky - Maxi Zink");
 
-    DiscordBot discordBot = new DiscordBot("ODA0NDg1NzMzOTA1NDY1Mzk0.YBNBuQ.EkZWQnJdGFJI3mqfmUFimTe5zl0");
+    DiscordBot discordBot = new DiscordBot("token");
     discordBot.setActivity(Activity.playing("hier"));
     discordBot.setStatus(OnlineStatus.DO_NOT_DISTURB);
 
